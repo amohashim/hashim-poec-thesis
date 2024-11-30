e5e1a92b9a888f7769eed55dcc619173c6c191a9
alihashim <ahashim449@gmail.com>
2024-11-30 12:59:26 -0500

    Initial commit

---
+++ simulate_preferences.jl
@@ -0,0 +1,21 @@
+module 
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+
+    """
+    Issue 1
+    Dimensionality: 2
+    Decomposed Questions: 5
+    Q1 -> 3 options
+    Q2 -> 3 options
+    Q3 -> 3 options
+    Q4 -> 4 options
+    Q5 -> 2 options
+    """
+
+
+
+
+
+end
