bd2944610c5370ed28719508dce16987749551ea
alihashim <ahashim449@gmail.com>
2024-11-30 18:28:54 -0500

    added demographic_simulation as its own file

--- simulation_package/SimulationPackage.jl
+++ simulation_package/SimulationPackage.jl
@@ -2,6 +2,7 @@ module HashimPoecThesisSimulationPackage
 
 export PreferenceSimulation
 
+include("demographic_simulation.jl")
 include("preference_simulation.jl")
 include("candidate_simulation.jl")
 include("elections_simulation.jl")
